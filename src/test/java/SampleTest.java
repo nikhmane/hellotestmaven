3c5a561b769cc1e1c088401ee25077fed39e3470
MANE <nikhil.b.mane@capgemini.com>
2018-05-28 15:21:19 +0530

    updated test file

--- src/test/java/SampleTest.java
+++ src/test/java/SampleTest.java
@@ -5,10 +5,18 @@ import org.junit.Test;
 public class SampleTest {
 
 	@Test
-	public void simpleTest() {
+	public void simpleTestPass() {
 		int a = 5;
 		int b = 10 ;
 		assertTrue(a+b == 15);
+		
+	}
+	
+	@Test
+	public void simpleTestFail() {
+		int a = 5;
+		int b = 10 ;
+		assertTrue(a+b == 20);
 		
 	}
 }
