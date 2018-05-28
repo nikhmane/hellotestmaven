b14b601b78b13ccf5866405ebbe42bd7f7f54359
MANE <nikhil.b.mane@capgemini.com>
2018-05-28 15:09:40 +0530

    added test file

---
+++ src/test/java/SampleTest.java
@@ -0,0 +1,14 @@
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class SampleTest {
+
+	@Test
+	public void simpleTest() {
+		int a = 5;
+		int b = 10 ;
+		assertTrue(a+b == 15);
+		
+	}
+}
